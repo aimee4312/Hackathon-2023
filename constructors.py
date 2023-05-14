58e72759740262744b9a0b7bb433dc5a25889e67
adiaz129 <adiaz129@calpoly.edu>
2023-05-14 08:45:47 -0700

    merged

--- constructors.py
+++ constructors.py
@@ -15,13 +15,13 @@ def spawn_p_tank(sw_settings, screen, worth):
     return Troop(sw_settings, screen, 240, .5, 20, 50, worth, A_TANK, True)
     
 def spawn_np_reg(sw_settings, screen, worth):
-    return Troop(sw_settings, screen, 100, 1, 20, 20, worth, E_WEAK, False)
+    return Troop(sw_settings, screen, 100, -1, 20, 20, worth, E_WEAK, False)
     
 def spawn_np_fast(sw_settings, screen, worth):
-    return Troop(sw_settings, screen, 40, 3, 20, 25, worth, E_FAST, False)
+    return Troop(sw_settings, screen, 40, -3, 20, 25, worth, E_FAST, False)
 
 def spawn_np_range(sw_settings, screen, worth):
-    return Troop(sw_settings, screen, 80, 1, 150, 20, worth, E_RANGE, False)
+    return Troop(sw_settings, screen, 80, -1, 150, 20, worth, E_RANGE, False)
     
 def spawn_np_tank(sw_settings, screen, worth):
-    return Troop(sw_settings, screen, 240, .5, 20, 50, worth, E_TANK, False)
+    return Troop(sw_settings, screen, 240, -.5, 20, 50, worth, E_TANK, False)
